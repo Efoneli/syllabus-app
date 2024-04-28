274453f3c7750c3bb482e0cf0415951d1f83df75
efoneli <abbaefoneli@gmail.com>
2024-04-28 14:27:19 +0100

    changes

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000', // Specify your frontend domain here
+    credentials: true, // Allow credentials (e.g., cookies, authorization headers)
+  });
   await app.listen(3030);
 }
 bootstrap();
