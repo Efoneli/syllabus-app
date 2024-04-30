abab2339f02f38217da08974396803ec03339039
efoneli <abbaefoneli@gmail.com>
2024-04-30 10:24:59 +0100

    changes

--- client/app/Dashboard/page.tsx
+++ client/app/Dashboard/page.tsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import { Carousel } from "flowbite-react";
+import arif from '../assests/arif.jpg'
+import Avatar from '../assests/Avatar.png'
+import brook from '../assests/brook.jpg'
+import christopher from '../assests/christopher.jpg'
+import joshua from '../assests/joshua.jpg'
+import window from '../assests/arif.jpg'
+import Image from "next/image";
 
-function page() {
+export default function Page() {
   return (
-    <div>
-      <div className='border border-gray-500 bg-[#f5f5f5] text-black'>
-        this is the main div for introduction
+    <div className="p-12">
+      <div>
+        <h1 className="text-3xl mb-4 text-gray-300">Welcome to WDAT Syllabus </h1>
+        <p className="text-md mb-4 text-gray-300">
+          Empower your learning journey with our comprehensive web development syllabus app. Whether you're a student, educator, or lifelong learner, WDAT is here to streamline your study experience and help you achieve your web dev goals.
+        </p>
       </div>
 
-      <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
-        <div className='border border-gray-500 bg-green-100 text-black'>
-          this is a half div
-        </div>
-        <div className='border border-gray-500 bg-pink-100 text-black'>
-          this is also a half div
-        </div>
+      <div className="h-60 sm:h-64 xl:h-80 2xl:h-96">
+        <Carousel>
+          <Image height={300} width={300} src={arif} alt="..." />
+          <Image height={300} width={300} src={joshua} alt="..." />
+          <Image height={300} width={300} src={brook} alt="..." />
+          <Image height={300} width={300} src={christopher} alt="..." />
+          <Image height={300} width={300} src={joshua} alt="..." />
+          <Image height={300} width={300} src={window} alt="..." />
+        </Carousel>
       </div>
     </div>
-  )
+  );
 }
-
-export default page
