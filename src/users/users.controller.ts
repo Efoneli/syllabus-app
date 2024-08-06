49ac298daa8029b79faf8476d84b67f53f70824a
efoneli <abbaefoneli@gmail.com>
2024-08-06 10:10:41 +0100

    user roles changes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ConflictException, NotFoundException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -11,6 +11,24 @@ export class UsersController {
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
+
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    const existingUser = await this.usersService.findByEmail(createUserDto.email);
+    if (existingUser) {
+      throw new ConflictException('User already exists');
+    }
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get('email/:email')
+async findByEmail(@Param('email') email: string) {
+  const user = await this.usersService.findByEmail(email);
+  if (!user) {
+    throw new NotFoundException('User not found');
+  }
+  return user;
+}
 
   @Get()
   findAll() {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,3 +1,82 @@
+// import { Injectable, ConflictException, NotFoundException } from '@nestjs/common';
+// import { CreateUserDto } from './dto/create-user.dto';
+// import { UpdateUserDto } from './dto/update-user.dto';
+// import { User } from './entities/user.entity';
+// import { InjectRepository } from '@nestjs/typeorm';
+// import { Repository } from 'typeorm';
+
+// @Injectable()
+// export class UsersService {
+//   constructor(
+//     @InjectRepository(User)
+//     private readonly usersRepository: Repository<User>,
+//   ) {}
+
+//   // async create(createUserDto: CreateUserDto) {
+//   //   const existingUser = await this.usersRepository.findOne({
+//   //     where: [{ email: createUserDto.email }, { sub: createUserDto.sub }],
+//   //   });
+
+//   //   if (existingUser) {
+//   //     throw new ConflictException('User already exists');
+//   //   }
+
+//   //   const user = this.usersRepository.create(createUserDto);
+//   //   await this.usersRepository.save(user);
+//   //   return user;
+//   // }
+
+
+//   async findByEmail(email: string) {
+//     return this.usersRepository.find(user => user.email === email);
+//   }
+
+//   async create(createUserDto: CreateUserDto) {
+//     const newUser = { id: Date.now(), ...createUserDto };
+//     this.usersRepository.push(newUser);
+//     return newUser;
+//   }
+
+//   // async create(createUserDto: CreateUserDto): Promise<User> {
+//   //   const user = this.usersRepository.create(createUserDto);
+//   //   return this.usersRepository.save(user);
+//   // }
+
+//   findAll() {
+//     return this.usersRepository.find();
+//   }
+
+//   findOne(id: number) {
+//     return this.usersRepository.findOneBy({ id });
+//   }
+
+//   async update(id: number, updateUserDto: UpdateUserDto) {
+//     const user = await this.usersRepository.findOneBy({ id });
+//     if (!user) {
+//       throw new NotFoundException('User not found');
+//     }
+//     user.name = updateUserDto.name ?? user.name;
+//     user.email = updateUserDto.email ?? user.email;
+//     user.sub = updateUserDto.sub ?? user.sub;
+//     user.isAdmin = updateUserDto.isAdmin ?? user.isAdmin;
+
+//     await this.usersRepository.save(user);
+//     return user;
+//   }
+
+//   async remove(id: number) {
+//     await this.usersRepository.delete(id);
+//   }
+// }
+
+
+
+
+
+
+
+
+
 import { Injectable, ConflictException, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -12,35 +91,33 @@ export class UsersService {
     private readonly usersRepository: Repository<User>,
   ) {}
 
-  // async create(createUserDto: CreateUserDto) {
-  //   const existingUser = await this.usersRepository.findOne({
-  //     where: [{ email: createUserDto.email }, { sub: createUserDto.sub }],
-  //   });
-
-  //   if (existingUser) {
-  //     throw new ConflictException('User already exists');
-  //   }
-
-  //   const user = this.usersRepository.create(createUserDto);
-  //   await this.usersRepository.save(user);
-  //   return user;
-  // }
-
+  async findByEmail(email: string): Promise<User | null> {
+    return this.usersRepository.findOneBy({ email });
+  }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
+    // Check if a user with the same email or sub already exists
+    const existingUser = await this.findByEmail(createUserDto.email);
+    if (existingUser) {
+      throw new ConflictException('User already exists');
+    }
+
+    // Create a new user entity
     const user = this.usersRepository.create(createUserDto);
+
+    // Save the new user entity
     return this.usersRepository.save(user);
   }
 
-  findAll() {
+  findAll(): Promise<User[]> {
     return this.usersRepository.find();
   }
 
-  findOne(id: number) {
+  findOne(id: number): Promise<User | null> {
     return this.usersRepository.findOneBy({ id });
   }
 
-  async update(id: number, updateUserDto: UpdateUserDto) {
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
     const user = await this.usersRepository.findOneBy({ id });
     if (!user) {
       throw new NotFoundException('User not found');
@@ -54,7 +131,7 @@ export class UsersService {
     return user;
   }
 
-  async remove(id: number) {
+  async remove(id: number): Promise<void> {
     await this.usersRepository.delete(id);
   }
 }
