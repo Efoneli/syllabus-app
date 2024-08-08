df42ef8fae499d5eae8a9784407c9034c085f453
efoneli <abbaefoneli@gmail.com>
2024-08-08 12:33:35 +0100

    urls added

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.enableCors({
-    origin: 'http://localhost:3000', // Specify your frontend domain here
+    origin: 'https://syllabus-app.vercel.app', // Specify your frontend domain here
     credentials: true, // Allow credentials (e.g., cookies, authorization headers)
   });
   await app.listen(3030);
