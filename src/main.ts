36021accea04753f75bdf7d12a1d9f3c37d050b3
efoneli <abbaefoneli@gmail.com>
2024-08-12 10:54:04 +0100

    fixes

--- src/main.ts
+++ src/main.ts
@@ -23,17 +23,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
-  // Enable CORS (Cross-Origin Resource Sharing)
   app.enableCors({
-    origin: 'https://syllabus-app.vercel.app/', // Specify your frontend domain here
+    origin: 'https://syllabus-app.vercel.app/',
     // origin: 'http://localhost:3000/', 
-    credentials: true, // Allow credentials (e.g., cookies, authorization headers)
+    credentials: true,
   });
 
-  // Retrieve the port from environment variables or use a default value
   const port = process.env.PORT || 3030;
 
-  // Start listening on the specified port
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
